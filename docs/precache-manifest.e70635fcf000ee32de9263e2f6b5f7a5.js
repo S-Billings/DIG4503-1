9d2933aa0aa2e7eae2ed1bf833519addb6ff5523
Dan Cox <dan.cox@videlais.com>
2020-04-03 22:58:29 -0400

    Uploading docs example

---
+++ docs/precache-manifest.e70635fcf000ee32de9263e2f6b5f7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b400287315b46ad09433e45ccdd76ce",
+    "url": "/DIG4503/index.html"
+  },
+  {
+    "revision": "d0211e160b8fe1f978fe",
+    "url": "/DIG4503/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a63c351800c277b6fe50",
+    "url": "/DIG4503/static/js/2.b4ce2c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DIG4503/static/js/2.b4ce2c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0211e160b8fe1f978fe",
+    "url": "/DIG4503/static/js/main.7a0afc40.chunk.js"
+  },
+  {
+    "revision": "cc3f6c2d47c8bbfa6564",
+    "url": "/DIG4503/static/js/runtime-main.502a9c22.js"
+  }
+]);
---
+++ reactdemo/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <p>This is a demo!</p>
+  );
+}
+
+export default App;
