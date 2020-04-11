4aca25564456b40f381a855fda976ee29ab5b824
Dan Cox <dan.cox@videlais.com>
2020-04-11 17:38:01 -0400

    Next.js Testing

---
+++ docs/_next/static/209-genWjvyI9BalxdP-O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/example":[a],"/index":[a]}}("static\u002Fchunks\u002F92617c97f65fffd67bf9cfca096216e8fa39d712.ecf64d41e48ed951acc2.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
